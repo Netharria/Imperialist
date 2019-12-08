8e868ee46ac7815a5a81862bfb081a5ec38783a5
Runtime.Ali <runtimeone@protonmail.com>
2019-12-08 18:51:01 -0500

    length calculations added

--- bot.py
+++ bot.py
@@ -13,6 +13,8 @@ async def ping(ctx):
 
 import random
 
+#8ball random answer
+
 @client.command(aliases=['8ball', 'test'])
 async def _8ball(ctx, *, question):
     responses = ['It is certain.',
@@ -36,27 +38,28 @@ async def _8ball(ctx, *, question):
                 'Outlook not so good.',
                 'Very doubtful.']
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+#temp conversions
 def c_f_conversion(start_temp):
-    new_temp = (start_temp * 9/5) + 3200
+    new_temp = (start_temp * 9/5) + 32
     return new_temp
 def f_c_conversion(start_temp):
-    new_temp = (start_temp - 3200) * 5/9
+    new_temp = (start_temp - 32) * 5/9
     return new_temp
 def k_c_conversion(start_temp):
-    new_temp = start_temp - 27315
+    new_temp = start_temp - 273.15
     return new_temp
 def c_k_conversion(start_temp):
-    new_temp = start_temp + 27315
+    new_temp = start_temp + 273.15
     return new_temp
 
-
+#temp Commands
 @client.command()
 async def temp(ctx, *, conversion):
     conversion_scrub = conversion.upper()
     conversion_split = conversion_scrub.split()
     start_temp = conversion_split[0]
     int_temp = int(start_temp[:-1])
-    int_temp *= 100
     if 'C' in start_temp:
         if 'F' in conversion_split[2]:
             new_temp = c_f_conversion(int_temp)
@@ -81,9 +84,109 @@ async def temp(ctx, *, conversion):
             degree = 'Fahrenheit'
     else:
         await ctx.send(f'The Correct format is Exp:`imp.temp 10F to C`')
-    await ctx.send(f'{start_temp} is {(int(new_temp)/100)} {degree}')
+    await ctx.send(f'{start_temp} is {new_temp:.2f} {degree}')
 
+#Length Conversions to meters
+def km_m_conversion(start_len):
+    new_len = start_len * 1000
+    return new_len
+def cm_m_conversion(start_len):
+    new_len = start_len / 100
+    return new_len
+def mm_m_conversion(start_len):
+    new_len = start_len / 1000
+    return new_len
+def mi_m_conversion(start_len):
+    new_len = start_len * 1609.344
+    return new_len
+def yd_m_conversion(start_len):
+    new_len = start_len * 0.9144
+    return new_len
+def ft_m_conversion(start_len):
+    new_len = start_len * 0.3048
+    return new_len
+def in_m_conversion(start_len):
+    new_len = start_len * .0254
+    return new_len
 
+#Length Conversions from meters
+def m_km_conversion(start_len):
+    new_len = start_len / 1000
+    return new_len
+def m_cm_conversion(start_len):
+    new_len = start_len * 100
+    return new_len
+def m_mm_conversion(start_len):
+    new_len = start_len * 1000
+    return new_len
+def m_mi_conversion(start_len):
+    new_len = start_len * 0.00062137119223733
+    return new_len
+def m_yd_conversion(start_len):
+    new_len = start_len * 1.0936132983377
+    return new_len
+def m_ft_conversion(start_len):
+    new_len = start_len * 3.2808398950131
+    return new_len
+def m_in_conversion(start_len):
+    new_len = start_len * 39.370078740157
+    return new_len
 
+#temp command
+@client.command(aliases=['len'])
+async def _len(ctx, *, conversion):
+    conversion_scrub = conversion.lower()
+    conversion_split = conversion_scrub.split()
+    start_len = conversion_split[0]
+    end_conv = conversion_split [2]
+    int_len = int(start_len[:-2])
+    new_len = 0
+    unit = ''
+    if 'km' in start_len:
+        meter_conv = km_m_conversion(int_len)
+    elif 'cm' in start_len:
+        meter_conv = cm_m_conversion(int_len)
+    elif 'mm' in start_len:
+        meter_conv = mm_m_conversion(int_len)
+    elif 'mi' in start_len:
+        meter_conv = mi_m_conversion(int_len)
+    elif 'yd' in start_len:
+        meter_conv = yd_m_conversion(int_len)
+    elif 'ft' in start_len:
+        meter_conv = ft_m_conversion(int_len)
+    elif 'in' in start_len:
+        meter_conv = in_m_conversion(int_len)
+    elif 'm' in start_len:
+        meter_conv = int(start_len[:-1])
+    else:
+        await ctx.send(f'The Correct format is Exp:`imp.len 10km to mi`  Valid units are km m cm mm mi yd ft in')
+    if end_conv == 'm':
+        new_len = meter_conv
+        unit = 'Meters'
+    else:
+        if 'km' in end_conv:
+            new_len = m_km_conversion(meter_conv)
+            unit = 'Kilometers'
+        elif 'cm' in end_conv:
+            new_len = m_cm_conversion(meter_conv)
+            unit = 'Centimeters'
+        elif 'mm' in end_conv:
+            new_len = m_mm_conversion(meter_conv)
+            unit = 'Millimeters'
+        elif 'mi' in end_conv:
+            new_len = m_mi_conversion(meter_conv)
+            unit = 'Miles'
+        elif 'yd' in end_conv:
+            new_len = m_yd_conversion(meter_conv)
+            unit = 'Yards'
+        elif 'ft' in end_conv:
+            new_len = m_ft_conversion(meter_conv)
+            unit = 'Feet'
+        elif 'in' in end_conv:
+            new_len = m_in_conversion(meter_conv)
+            unit = 'Inches'
+        else:
+            await ctx.send(f'The Correct format is Exp:`imp.len 10km to mi`   Valid units are km m cm mm mi yd ft in')
+    await ctx.send(f'{start_len} is {new_len:.4f} {unit}')
 
 client.run('')
